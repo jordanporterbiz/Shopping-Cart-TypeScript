fad9de00089b17cd0f82ab1990ce5470eb089248
Jordan Porter <53372392+JordanPorterDev@users.noreply.github.com>
2021-12-04 22:08:03 +0000

    adding cart handler functions

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,35 @@ import { Drawer, LinearProgress, Grid, Badge} from '@material-ui/core';
 import { AddShoppingCart } from '@material-ui/icons';
 // Styles
 //import Wrapper from './App.styles'
+// Types 
+export type CartItemType = {
+  id: number; 
+  category: string;
+  description: string;
+  image: string; 
+  price: number;
+  title: string;
+  amount: number;
+
+}
+
+const getProducts = async (): Promise<CartItemType[]> => 
+await (await fetch('https://fakestoreapi.com/products')).json()
+   
+
+const getTotalItems = () => null
+
+const handleAddToCart = () => null
+
+const handleRemoveFromCart = () => null
+
+
+
 
 const App = () => {
+
+  const { data, isLoading, error } = useQuery<CartItemType[]>('products', getProducts)
+  console.log(data)
   return (
     <div className="App">
       <h1>Hello Jordan!</h1>
